f63b24eea14a14be05046b99e4a8e1da2d851235
Emmanuezdj <133866276+Emmanuezdj@users.noreply.github.com>
2024-10-31 16:19:11 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+# app.py
+from flask import Flask,render_template,redirect,url_for,request
+
+app = Flask(__name__)
+
+
+
+
+@app.route('/')
+def home():
+	return render_template("index.html")
+
+
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+	username=request.form.get("username")
+	password=request.form.get("password")
+	return "Please wait for 24hrs generating"
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
